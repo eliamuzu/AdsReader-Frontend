62607f0aedcd426cdfb576c88a52970c9b561f62
eliamuzu <kofiprince375@gmail.com>
2025-11-28 17:32:53 +0000

    Update FilterProvider to include insightsData state management

--- src/contexts/FilterContext.jsx
+++ src/contexts/FilterContext.jsx
@@ -3,10 +3,11 @@ import React, { createContext, useContext, useState } from 'react';
 const FilterContext = createContext();
 
 export function FilterProvider({ children }) {
-  const [selectedFilter, setSelectedFilter] = useState('All');
+  const [selectedFilter, setSelectedFilter] = useState();
+  const [insightsData, setInsightsData] = useState(null);
 
   return (
-    <FilterContext.Provider value={{ selectedFilter, setSelectedFilter }}>
+    <FilterContext.Provider value={{ selectedFilter, setSelectedFilter, insightsData, setInsightsData }}>
       {children}
     </FilterContext.Provider>
   );
